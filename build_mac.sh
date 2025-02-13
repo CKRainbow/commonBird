4f1148ec3daab67d111166e91e1f51f35496b8f0
CK Rainbow <nanashichi@proton.me>
2025-02-13 19:12:40 +0800

    macos x64 and arm64

--- build_mac.sh
+++ build_mac.sh
@@ -16,6 +16,7 @@ pyinstaller --name commonBird \
     --exclude-module numpy \
     --exclude-module textual-dev \
     --exclude-module pyinstaller \
+    --onefile \
     cli.py
 
 npm i markdown-to-html-cli -g
@@ -24,14 +25,19 @@ markdown-to-html -i README.md -o dist/commonBird/README.html
 
 python taxon_map_preview.py --map_file ch4_to_eb_taxon_map.json --output_path dist/commonBird/taxon_map_preview.html
 
+mkdir dist/commonBird_
+
+mv dist/commonBird dist/commonBird_/
+mv dist/commonBird_ dist/commonBird
+
 cp -r res dist/commonBird/res
 cp -r database dist/commonBird/database
 cp README.md dist/commonBird
 cp changelog.md dist/commonBird
 
-touch dist/commonBird/fix_for_mac.sh
-echo "#!/bin/bash" >> dist/commonBird/fix_for_mac.sh
-echo "xattr -d com.apple.quarantine dist/commonBird/_internal/Python.framework" >> dist/commonBird/fix_for_mac.sh
+# touch dist/commonBird/fix_for_mac.sh
+# echo "#!/bin/bash" >> dist/commonBird/fix_for_mac.sh
+# echo "xattr -d com.apple.quarantine _internal/Python.framework" >> dist/commonBird/fix_for_mac.sh
 
 # compress the build
 tar cvzf commonBird_mac_x64.tar.gz dist/commonBird/
